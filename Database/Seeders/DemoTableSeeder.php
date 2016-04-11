0b07d1514ab07b40dc06201bff668ca7b6d1dc90
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-11 22:44:53 +0200

    updated DemoSeeder

--- Database/Seeders/DemoTableSeeder.php
+++ Database/Seeders/DemoTableSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Modules\Blog\Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Modules\Core\Traits\Factory\useFactories;
+use Modules\Menu\Entities\Menu;
 
 class DemoTableSeeder extends Seeder
 {
@@ -19,6 +21,33 @@ class DemoTableSeeder extends Seeder
     {
         Model::unguard();
         DB::table('blog__articles')->delete();
-        $this->factory(\Modules\Blog\Entities\Article::class, 12)->create();
+        $this->factory(\Modules\Blog\Entities\Article::class, 12)
+            ->create()
+            ->each(function($article) {
+                $faker = Factory::create();
+                if($faker->boolean(60)) {
+                    $image = $faker->image('/tmp', 1920, 1080);
+                    $article->clearMediaCollection('cover');
+                    $article->addMedia($image)->toMediaLibrary('cover');
+                }
+
+                $activity = $article->activities->first();
+                $activity->update([
+                    'created_at' => $start = $faker->dateTimeThisYear,
+                    'updated_at' => $faker->dateTimeBetween($start),
+                ]);
+            });
+
+        $this->createMenuEntry();
+    }
+
+    /**
+     *
+     */
+    private function createMenuEntry()
+    {
+        if ($main = Menu::root()->where(['name' => 'Main'])->first()) {
+            $main->children()->create(['name' => 'Blog', 'url' => 'blog', 'active' => true]);
+        }
     }
 }
