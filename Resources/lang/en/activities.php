bc902f3a71cf4141e762dc42240e05954e627927
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-27 22:19:58 +0200

    working on localization

---
+++ Resources/lang/en/activities.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'created_object' => [
+        'summary' => '{{:user}} published an article, <a href=":url">:title</a>',
+    ],
+];
--- Resources/views/backend/activities/created_article.blade.php
+++ Resources/views/backend/activities/created_article.blade.php
@@ -5,7 +5,14 @@
         </div>
         <div class="content">
             <div class="summary">
-                <a href="#">{{$activity->user->present()->fullname}}</a> published an article, <a href="{{route('blog.show', $activity->subject->slug)}}">{{$activity->subject->title}}</a>
+                {!!  trans(
+                    'blog::activities.created_object.summary',
+                    [
+                        'user' => $activity->user->present()->fullname,
+                        'title' => $activity->subject->title,
+                        'url' => route('blog.show', $activity->subject->slug),
+                    ]
+                    ) !!}
                 <div class="date">
                     {{$activity->created_at->diffForHumans()}}
                 </div>
@@ -15,7 +22,7 @@
             </div>
             <div class="meta">
                 <a class="info" target="_blank" href="{{route('blog.show', $activity->subject->slug)}}">
-                    <i class="info icon"></i> Read more
+                    <i class="info icon"></i> {{trans('core::elements.action.read more')}}
                 </a>
             </div>
         </div>
