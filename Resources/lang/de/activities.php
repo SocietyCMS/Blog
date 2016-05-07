6f1784e82bd0dd21bfade32c8906ddd239ccb85f
Ralph Huwiler <ralph@huwiler.rocks>
2016-05-07 20:23:58 +0200

    german localization

---
+++ Resources/lang/de/activities.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'created_object' => [
+        'summary' => ':user hat einen neuen Artikel veröffentlicht:  <a href=":url">:title</a>',
+    ],
+];
---
+++ Resources/lang/de/blog.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'title' => [
+        'blog'           => 'Blog',
+        'article'        => 'Artikel',
+        'photo'          => 'Foto',
+        'photos'         => 'Fotos',
+        'cover'          => 'Cover',
+        'covers'         => 'Covers',
+        'file'           => 'Datei',
+        'files'          => 'Dateien',
+        'all blog posts' => 'Alle Blogeinträge',
+    ],
+    'table' => [
+        'title'     => 'Titel',
+        'author'    => 'Autor',
+        'modified'  => 'Bearbeitet',
+        'status'    => 'Status',
+    ],
+    'form' => [
+        'title'   => 'Titel',
+        'slug'    => 'Slug',
+        'content' => 'Inhalt'
+    ],
+    'state' => [
+        'pin'     => 'Anpinnen',
+        'pinned'  => 'Gepinnt'
+    ],
+    'validation' => [
+        'attributes' => [
+            'title' => 'titel',
+            'body'  => 'inhalt',
+        ],
+    ],
+    'messages' => [
+        'no articles' => 'Noch kein Blogeinträge. Du kannst aber einen erstellen mit der Schaltfäche oben.',
+    ],
+];
